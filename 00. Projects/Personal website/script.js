2636b2a4a75c229c8029d6905dea5756327a306e
Abdallah Beraidais <abdallah.beraidais@gmail.com>
2026-09-09 23:12:55 +0100

    HTML

--- 00. Projects/Personal website/script.js
+++ 00. Projects/Personal website/script.js
@@ -2,16 +2,16 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space")
+favouriteMovieGenre("regular")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("regular")
 
 // light, dark
-favouriteMode("light")
+favouriteMode("dark")
 
 // sharp, soft, round
-favouriteEdgeStyle("sharp")
+favouriteEdgeStyle("round")
 
 
 
